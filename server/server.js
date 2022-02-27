82d65364ce7ea92ec838f763cc02df284b5b24ea
gadmregon <jim231998@gmail.com>
2022-02-27 18:16:38 +0300

    Hi

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3500;
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const mysql = require("mysql2");
 
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
